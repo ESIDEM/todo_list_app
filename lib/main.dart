8c65c45ab47c242f5e011d6da430029592543fec
Sam Esidem <samesidem@gmail.com>
2020-01-22 15:01:34 +0100

    ListView Added for Todo items

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,64 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   return runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "ToDo List Application",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("ToDo List App"),
-        ),
-        body: Center(child: Text("List items here"),),
+    return MaterialApp(title: "ToDo List Application", home: TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TodoListState();
+  }
+}
+
+class TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ToDo List App"),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodoItem,
+        tooltip: "Add Item",
+        child: Icon(Icons.add),
       ),
     );
   }
 
+  // This will be called each time the + button is pressed
+  void _addTodoItem() {
+    // Putting our code inside "setState" tells the app that our state has changed, and
+    // it will automatically re-render the list
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item ' + index.toString());
+    });
+  }
 
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return ListTile(title: Text(todoText));
+  }
 }
